6d4d48eddee65e8d02eae236e27d01eb39516d10
Titov Ilya <titov1608@gmail.com>
2025-03-21 10:11:02 +0300

    chuv_translate

--- app/uti/translate.py
+++ app/uti/translate.py
@@ -3,9 +3,10 @@ import json
 
 
 class Translator:
-    def __init__(self, iam_token, folder_id):
+    def __init__(self, iam_token, folder_id, default_language="cv"):
         self.iam_token = iam_token
         self.folder_id = folder_id
+        self.default_language = default_language
         self.url = "https://translate.api.cloud.yandex.net/translate/v2/translate"
         self.headers = {
             "Content-Type": "application/json",
@@ -13,9 +14,14 @@ class Translator:
         }
         self.translated_cache = {}
 
-    async def translate(self, text, target_language="cv"):
-        if text in self.translated_cache:
-            return self.translated_cache[text]
+    async def translate(self, text, target_language=None):
+        if target_language is None:
+            target_language = self.default_language
+
+        # Используем кэш перевода, если доступен
+        cache_key = f"{text}-{target_language}"
+        if cache_key in self.translated_cache:
+            return self.translated_cache[cache_key]
 
         data = {
             "folder_id": self.folder_id,
@@ -28,7 +34,7 @@ class Translator:
                 if response.status == 200:
                     result = await response.json()
                     translated_text = result["translations"][0]["text"]
-                    self.translated_cache[text] = translated_text
+                    self.translated_cache[cache_key] = translated_text
                     return translated_text
                 else:
                     print(f"Ошибка перевода: {response.status}")
